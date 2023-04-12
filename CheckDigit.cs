ca17e75d76c93edca8ab072f520a25014b73c942
Apollo <88677238+ApolloDraco@users.noreply.github.com>
2023-04-12 02:31:02 -0400

    Update CheckDigit.cs

--- CheckDigit.cs
+++ CheckDigit.cs
@@ -1,35 +1,41 @@
+/*
+ * This application takes input from the user in the form of a four-digit account number.
+ * It then checks if the account number is valid by applying a simple check digit algorithm.
+ * The check digit algorithm involves dividing the first three digits of the account number by a constant value of 7 to obtain the remainder.
+ * If the remainder matches the last digit of the account number, then the account number is considered valid; otherwise, it is invalid.
+ * If the user enters an invalid account number that is not exactly four digits long, the program displays an error message.
+ * If the account number is valid, the program outputs a message indicating that the account number is valid.
+ */
+
 using System;
-using static System.Console;
+
 class CheckDigit
 {
+    static void Main()
+    {
+        const int DIGITS = 4;
+        const int CHECK_FACTOR = 7;
 
-static void Main()
-{
-int acctNum;
-string acctNumString;
-const int DIGITS = 4;
-int firstThree;
-int lastOne;
-const int CHECK_FACTOR = 7;
-int remainder;
-Write("Enter a four-digit account number >> ");
-acctNumString = ReadLine();
-if(acctNumString.Length != DIGITS)
-WriteLine($"Account number invalid - it must have {DIGITS} digits");
-else
-{
-acctNum = Convert.ToInt32(acctNumString);
-firstThree = acctNum / 10;
-lastOne = acctNum % 10;
-remainder = firstThree % CHECK_FACTOR;
-if (lastOne == remainder)
-WriteLine("The account number is valid");
-else 
-WriteLine("Invalid");
+        Console.Write("Enter a four-digit account number >> ");
+        string acctNumString = Console.ReadLine();
 
+        if (acctNumString.Length != DIGITS || !int.TryParse(acctNumString, out int acctNum))
+        {
+            Console.WriteLine($"Account number invalid - it must have {DIGITS} digits");
+            return;
+        }
 
+        int firstThree = acctNum / 10;
+        int lastOne = acctNum % 10;
+        int remainder = firstThree % CHECK_FACTOR;
 
-}
-
-}
+        if (lastOne == remainder)
+        {
+            Console.WriteLine("The account number is valid");
+        }
+        else 
+        {
+            Console.WriteLine("Invalid");
+        }
+    }
 }
